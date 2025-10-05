b705a4f1d269df943c104c0f427750035ba3ee47
1121gbps <putrabima.media@gmail.com>
2025-10-05 17:23:17 +0000

    fix: update README and config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
+  images: {
+    domains: [
+      "hitulqisamyhdeenrphk.supabase.co"  // don't forget to replace with your own supabase here if you managed to fork / clone 
+    ],
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
+
 }
+
 export default nextConfig;
